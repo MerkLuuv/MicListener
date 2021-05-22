1c6fbfa7d98c39ab7319e4edf6edc09f932ec8a6
MerkLuuv <84462618+MerkLuuv@users.noreply.github.com>
2021-05-22 22:32:30 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,30 +1,68 @@
-/**
- * @name Frequency Spectrum
- * @description <p>Visualize the frequency spectrum of live audio input.</p>
- * <p><em><span class="small"> To run this example locally, you will need the
- * <a href="http://p5js.org/reference/#/libraries/p5.sound">p5.sound library</a>
- * and a running <a href="https://github.com/processing/p5.js/wiki/Local-server">local server</a>.</span></em></p>
- */
-let mic, fft;
+let fft;
+let input;
+let startButton;
 
 function setup() {
-  createCanvas(710, 400);
-  noFill();
-
-  mic = new p5.AudioIn();
-  mic.start();
-  fft = new p5.FFT();
-  fft.setInput(mic);
+  let cnv = createCanvas(windowWidth, windowHeight);
+  startButton = createButton('Start!');
+  startButton.position(width / 2, height / 2);
+  startButton.mousePressed(async () => {
+    startButton.remove();
+    await userStartAudio();
+    // Oddly enough the await above does not block until the user clicks "Allow".
+    // However, this does still seem to consistently work.
+    fft = new p5.FFT();
+    input = new p5.AudioIn();
+    input.start();
+    fft.setInput(input);
+  });
 }
 
 function draw() {
-  background(200);
+  background('black');
+  if (fft && input) {
+    let w = width / 2;
+    drawSpectrumGraph(0, 0, w, height);
+    drawWaveformGraph(w, 0, w, height);
+  }
+}
 
+// Graphing code adapted from https://jankozeluh.g6.cz/index.html by Jan Koželuh
+function drawSpectrumGraph(left, top, w, h) {
   let spectrum = fft.analyze();
 
+  stroke('limegreen');
+  fill('darkgreen');
+  strokeWeight(1);
+
   beginShape();
-  for (i = 0; i < spectrum.length; i++) {
-    vertex(i, map(spectrum[i], 0, 255, height, 0));
+  vertex(left, top + h);
+
+  for (let i = 0; i < spectrum.length; i++) {
+    vertex(
+      left + map(log(i), 0, log(spectrum.length), 0, w),
+      top + map(spectrum[i], 0, 255, h, 0)
+    );
   }
+
+  vertex(left + w, top + h);
+  endShape(CLOSE);
+}
+
+function drawWaveformGraph(left, top, w, h) {
+  let waveform = fft.waveform();
+
+  stroke('limegreen');
+  noFill();
+  strokeWeight(1);
+
+  beginShape();
+
+  for (let i = 0; i < waveform.length; i++) {
+    let x = map(i * 5, 0, waveform.length, 0, w);
+    let y = map(waveform[i], -1, 2, h / 10 * 8, 0);
+    vertex(left + x, top + y);
+  }
+
   endShape();
 }
